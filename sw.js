4530b1bf562caec6607c89b707abb49335766a4e
transportesletelierltda <transportesletelierltda@gmail.com>
2026-08-24 15:20:54 -0400

    Actualizar cache de la app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tl-cache-v2';
+const CACHE='tl-cache-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
